b7e2e9e6c816fd69767e802f6227d9bc525d47c5
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2024-11-10 14:21:42 +0100

    arreglos

--- Proyectos/Tarea_tema2/Ejercicios_de_Practica1/TEMA2_E1.java
+++ Proyectos/Tarea_tema2/Ejercicios_de_Practica1/TEMA2_E1.java
@@ -1,40 +1,42 @@
 import java.util.Scanner;
+
 public class TEMA2_E1 {
     public static void main(String[] args) {
-        //declaracion de variables
+        // declaracion de variables
         int numero;
 
-        //declaracion Scanner
-        Scanner sc = new Scanner (System.in);
+        try (// declaracion Scanner
+                Scanner sc = new Scanner(System.in)) {
+            System.out.println("introduce el numero entre 0 y 9 ");
+            numero = sc.nextInt();
+            sc.close();
+        }
 
-        System.out.println("introduce el numero entre 0 y 9 ");
-        numero = sc.nextInt();
-
-        //mostrar resultado
+        // mostrar resultado
         if (numero == 1) {
             System.out.println("uno");
-        } else if (numero == 2){
+        } else if (numero == 2) {
             System.out.println("dos");
-        } else if (numero == 3){
+        } else if (numero == 3) {
             System.out.println("tres");
-        } else if (numero == 4){
+        } else if (numero == 4) {
             System.out.println("cuatro");
-        } else if (numero == 5){
+        } else if (numero == 5) {
             System.out.println("cinco");
-        } else if (numero == 6){
+        } else if (numero == 6) {
             System.out.println("seis");
-        } else if (numero == 7){
+        } else if (numero == 7) {
             System.out.println("siete");
-        } else if (numero == 8){
+        } else if (numero == 8) {
             System.out.println("ocho");
-        } else if (numero == 9){
+        } else if (numero == 9) {
             System.out.println("nueve");
-        } else if (numero == 0){
+        } else if (numero == 0) {
             System.out.println("cero");
         } else {
             System.out.println("error");
         }
 
     }
-    
+
 }
--- Proyectos/Tarea_tema2/Ejercicios_de_Practica2/TEMA2_P2_E6for.java
+++ Proyectos/Tarea_tema2/Ejercicios_de_Practica2/TEMA2_P2_E6for.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
+
 public class TEMA2_P2_E6for {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        final int pinCorrecto = 2024;
-        int pin, contador = 3;
+        try (Scanner sc = new Scanner(System.in)) {
+            final int pinCorrecto = 2024;
+            int pin, contador = 3;
 
-        System.out.println("introduce el pin");
-        pin = sc.nextInt();
-            
-        for(; pin != pinCorrecto && contador > 0; contador --){
-           System.out.println("el pin es incorrecto");
-           pin = sc.nextInt();
-        } 
-        if (pin == pinCorrecto) {
-            System.out.println("el pin es correcto");
-        }else
-            System.out.println("te has quedado sin intentos");
-        
+            System.out.println("introduce el pin");
+            pin = sc.nextInt();
+
+            for (; pin != pinCorrecto && contador > 0; contador--) {
+                System.out.println("el pin es incorrecto");
+                pin = sc.nextInt();
+            }
+            if (pin == pinCorrecto) {
+                System.out.println("el pin es correcto");
+            } else
+                System.out.println("te has quedado sin intentos");
+            sc.close();
+        }
     }
 }
