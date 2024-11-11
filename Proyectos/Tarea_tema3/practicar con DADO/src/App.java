29ed91f5a40a3d1a401921a6004a43457bd0080e
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2024-11-11 11:01:54 +0100

    arreglo dado

--- Proyectos/Tarea_tema3/practicar con DADO/src/App.java
+++ Proyectos/Tarea_tema3/practicar con DADO/src/App.java
@@ -1,8 +1,8 @@
 import aguadulce.Dado;
 
-public class App {
+public class APP {
     public static void main(String[] args) throws Exception {
-        Dado miDado = new Dado(12);
+        Dado miDado = new Dado(6);
         String resultado;
         for (int i = 0; i < 20; i++) {
             resultado = miDado.lanzar();
