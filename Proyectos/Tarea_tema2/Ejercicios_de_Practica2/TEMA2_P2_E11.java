fab93394b4abbeddbac75b1ad8cb6feb8f5f1142
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2024-11-12 14:06:21 +0100

    arreglos clase

---
+++ Proyectos/Tarea_tema2/Ejercicios_de_Practica2/TEMA2_P2_E11.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class TEMA2_P2_E11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
--- Proyectos/Tarea_tema3/practicar con DADO/src/ejercicio3_T3.java
+++ Proyectos/Tarea_tema3/practicar con DADO/src/ejercicio3_T3.java
@@ -4,34 +4,34 @@ public class ejercicio3_T3 {
     public static void main(String[] args) {
         Dado miDado = new Dado();
         String resultado;
+
         int acumulador = 0;
         do {
             resultado = miDado.lanzar();
             System.out.println(resultado);
             // if(resultado == SEIS){}
-            if (resultado.contentEquals("SEISSEIS")) {
+            if (resultado.contentEquals("SEIS")) {
                 acumulador = acumulador + 1;
                 // acumulador++;
                 // acumulador +=1;
-            } else if (resultado.contentEquals("CINCOCINCO")) {
-                acumulador = acumulador + 1;
-
-            } else if (resultado.contentEquals("CUATROCUATRO")) {
-                acumulador = acumulador + 1;
-
-            } else if (resultado.contentEquals("TRESTRES")) {
-                acumulador = acumulador + 1;
-
-            } else if (resultado.contentEquals("DOSDOS")) {
-                acumulador = acumulador + 1;
-
-            } else if (resultado.contentEquals("UNOUNO")) {
-                acumulador = acumulador + 1;
-
             } else {
                 acumulador = 0;
+
             }
-        } while (acumulador < 1);
+            if (resultado.contentEquals("cinco")) {
+                acumulador = acumulador + 1;
+                // acumulador++;
+                // acumulador +=1;
+            } else {
+                acumulador = 0;
+
+            }
+        } while (acumulador < 2);
+
+        System.out.println("numero de lanzamientos " + miDado.getNumeroLanzamientos());
+        for (int i = 1; i <= miDado.getNumeroCaras(); i++) {
+            System.out.println("numero de veces de la cara " + i + ": " + miDado.getNumeroVecesCara(i));
+        }
 
     }
 
