d655a764ed151baa0200cebd7c7adb528976437c
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2024-11-18 20:34:46 +0100

    lenguaje

--- Proyectos/Tarea_tema3/Montes_Manzano_Moises_Tarea03/Montes_Manzano_Moises_Tarea03/src/ejercicio1.java
+++ Proyectos/Tarea_tema3/Montes_Manzano_Moises_Tarea03/Montes_Manzano_Moises_Tarea03/src/ejercicio1.java
@@ -1,12 +1,22 @@
-import aguadulce.Bombo;
+import aguadulce.*;
 import java.util.Scanner;
 
 public class ejercicio1 {
     public static void Bombo1() {
         Scanner sc = new Scanner(System.in);
-        Bombo miBombo = new Bombo(30);
-        int numero1, numero2, numero3, numero4, numero5, bola_fuera;
+        Bombo miBombo = new Bombo();
+        int numero1, numero2, numero3, numero4, numero5, bola_fuera, eleccion;
 
+        do {
+            mostrarMenu();
+            eleccion = sc.nextInt();
+
+        } while (eleccion != 0);
+        try {
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
         System.out.println("introduce el primer numero ");
         numero1 = sc.nextInt();
         System.out.println("introduce el segundo numero ");
@@ -17,7 +27,6 @@ public class ejercicio1 {
         numero4 = sc.nextInt();
         System.out.println("introduce el quinto numero ");
         numero5 = sc.nextInt();
-
         miBombo.extraerBola();
         bola_fuera = miBombo.extraerBola();
         System.out.println(miBombo.getCapacidad());
@@ -43,5 +52,11 @@ public class ejercicio1 {
         }
         System.out.println("ya han salido mis numeros");
         sc.close();
+    }
+
+    private static void mostrarMenu() {
+        System.out.println("BOMBO\n" +
+                "-----------\n" +
+                "Elije una opcion\n1.-30 - Bolas\n2.-50 - Bolas\n3.-70 - Bolas\n4.-90 - Bolas\n0.Salir");
     }
 }
