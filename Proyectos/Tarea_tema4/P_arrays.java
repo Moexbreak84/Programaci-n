f50e4485cbebb47c41492573c7aff8b119bb8503
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2024-11-19 21:04:28 +0100

    arrays

--- Proyectos/Tarea_tema4/P_arrays.java
+++ Proyectos/Tarea_tema4/P_arrays.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class arrays {
+public class P_arrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] numeros;
