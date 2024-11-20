2a8fb91915e928559b2f30b384746197dd544917
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2024-11-20 21:57:30 +0100

    practicando arrays

---
+++ Proyectos/Tarea_tema4/ejercicio4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[20];
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("introduce los 20 numeros de un solo digito debes repetir numeros ");
+            numeros[i] = sc.nextInt();
+
+        }
+        if (numeros [] = 7) {
+            System.out.println("estos son todos los numeros 7 que hay guardados " + numeros == 7);
+        }else if(numeros < 7){
+
+        }
+        sc.close();
+    }
+
+}
---
+++ Proyectos/Tarea_tema4/ejercicio5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ejercicio5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] numeros = new int[9];
+        for(int i = 0; i <= numeros.length; i++){
+
+        }
+        if(numeros[9] == numeros[9]) {
+            numeros[9] * 2;
+            System.out.println("Estos son los numeros multiplicados x 2 " + numeros);
+        }else{
+
+        }
+        sc.close();
+    }
+}
