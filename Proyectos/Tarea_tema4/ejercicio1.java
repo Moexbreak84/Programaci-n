3340446d1cc5ea3225d24dfd8b9dabf442cbe73d
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2024-11-20 13:05:32 +0100

    arrays y workspace

---
+++ Proyectos/Tarea_tema4/ejercicio1.java
@@ -0,0 +1,13 @@
+public class ejercicio1 {
+    public static void main(String[] args) {
+        int[] numeros = new int[10];
+        numeros[0] = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = 0;
+        }
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+    }
+
+}
