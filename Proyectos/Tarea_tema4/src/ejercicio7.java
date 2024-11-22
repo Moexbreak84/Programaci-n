99ddf06564496122a07877884f95dce19e76d66a
Moexbreak84 <mmontesmanzano.ieslapuebla@gmail.com>
2024-11-22 23:40:22 +0100

    char

--- Proyectos/Tarea_tema4/src/ejercicio7.java
+++ Proyectos/Tarea_tema4/src/ejercicio7.java
@@ -7,7 +7,7 @@ public class ejercicio7 {
         }
         int contadorA = 0;
         for (char letra : letras) {
-            if (letras == a || letras == A) {
+            if (letras == 'a' || letras == 'A') {
                 System.out.println("estas son las a y A que hay en el vector " + letras[letra]);
                 contadorA++;
             } else {
